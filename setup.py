fc3a724c631d3c7195ecf06da142072301354652
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-06-07 09:17:10 -0500

    Added bs4 missing requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='google_survey',
     packages=['google_survey'],
-    install_requires=['gspread', 'oauth2client', 'pandas', 'fire']
+    install_requires=['gspread', 'oauth2client', 'pandas', 'fire', 'bs4']
 )
