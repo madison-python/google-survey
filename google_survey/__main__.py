d4896161b85384dc56aad705a1422a5a6f9cdb3e
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-06-07 09:21:51 -0500

    Fixed bug in how I was using fire to generate the CLI

--- google_survey/__main__.py
+++ google_survey/__main__.py
@@ -2,13 +2,12 @@ import sys
 import fire
 from .all import get
 
-class GoogleSurveyCLI:
-    def get(self, survey_info_yaml, output=None):
-        responses = get(survey_info_yaml)
-        if output is None:
-            output = sys.stdout
-        responses.to_csv(output, index=False)
+
+def download_survey(survey_info_yaml, output=None):
+    responses = get(survey_info_yaml)
+    if output is None:
+        output = sys.stdout
+    responses.to_csv(output, index=False)
 
 if __name__ == '__main__':
-    cli = GoogleSurveyCLI()
-    fire.Fire(cli)
+    fire.Fire(download_survey)
